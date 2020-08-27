b04c7dd666a587df1573743eaca58f6dc48a0f9b
Pulsar <62395480+Woodnet@users.noreply.github.com>
2020-08-27 18:05:34 +0200

    Update main_index.js

--- main_index.js
+++ main_index.js
@@ -4,15 +4,9 @@ var auto_refresh = setInterval(
           $('#content1').load('chatindex.html');
         }, 1000);
 function CopyFunction() {
-          /* Get the text field */
           var copyText = document.getElementById("encryptionkey");
-          /* Select the text field */
           copyText.select();
-          copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-          /* Copy the text inside the text field */
+          copyText.setSelectionRange(0, 99999); 
           document.execCommand("copy");
-
-          /* Alert the copied text */
           alert("Copied the Encryption Key!");
 } 
