f6212587d51725178e76d0a6aa790d230b7cb15f
Pulsar <62395480+Woodnet@users.noreply.github.com>
2020-08-29 22:38:12 +0200

    Update REPclient.py

--- REPclient.py
+++ REPclient.py
@@ -16,6 +16,7 @@ y = Style.BRIGHT + Fore.YELLOW
 #
 #encryption-decryption
 key = input("Encrytion-Key>> ")
+nickname = input("Nickname>> ")
 f = Fernet(key.encode())
 #
 
@@ -53,42 +54,56 @@ def send(event=None):
         client_socket.close()
         top.quit()
 
+def sendnickname(nickname):
+    sys.stdout.write(w+"\r["+g+"INFO"+w+"]"+g+" Nickname wird verschlüsselt..")
+    sys.stdout.flush()
+    pkt = nickname.encode()
+    packet = f.encrypt(pkt)
+    print(w+"VERSCHLÜSSELT")
+    client_socket.send(packet)
+    if (nickname == "+quit+"):
+        print(w+"["+g+"INFO"+w+"]"+g+" Verbindung wird geschlossen")
+        client_socket.close()
+        top.quit()
+    
+
 def on_closing(event=None):
     my_msg.set("+quit+")
     send()
+    quit()
 
 top = tkinter.Tk()
 top.title("CHAT")
-top.configure(bg="gray18")
+top.configure(bg="black")
 top.geometry("1300x780")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()
-my_msg.set(" ")
+my_msg.set("")
 scrollbar = tkinter.Scrollbar(messages_frame)
-h = tkinter.Label(top,bg="gray18")
+h = tkinter.Label(top,bg="black")
 h.pack()
-msg_list = tkinter.Listbox(messages_frame, height=25,width=140,bd=6,relief="groove",highlightthickness=3,highlightcolor="whitesmoke",selectbackground="black",font="Arial 16",fg="white",bg="gray18", yscrollcommand=scrollbar.set)
+msg_list = tkinter.Listbox(messages_frame,highlightbackground="black",height=25,width=140,
+selectbackground="blue",font="Arial 13",fg="white",
+bg="black",bd=0,
+yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
-h = tkinter.Label(top,bg="gray18")
-h.pack()
-entry_field = tkinter.Entry(top, width=50,font="Helvetica 23",bg="gray15",fg="white",textvariable=my_msg)
-h = tkinter.Label(top,bg="gray18")
-h.pack()
+entry_field = tkinter.Entry(top, width=105,font="Courier 15",bg="white",
+bd=1,highlightbackground="white",fg="black",textvariable=my_msg)
 entry_field.bind("<Return>", send)
-entry_field.pack()
+entry_field.place(x=10,y=700)
 #send_button = tkinter.Button(top,font="Arial 20",bg="gray18", fg="white",text="Senden", command=send)
 #send_button.pack()
 top.protocol("WM_DELETE_WINDOW", on_closing)
 init()
-HOST = "localhost" #"3.21.60.148"-Ngrok (default)
+HOST = "192.168.81.128" #"3.21.60.148"-Ngrok (default)
 init()
-print(w+"\n\n"+r+"CHAT_"+g+"Client")
+print(w+"\n\n"+r+"CHAT"+g+"Client")
 print(w+"--> Version 1.0 \n--> Autor: Pulsar\n\n")
-PORT = 1337
+PORT = 1330
 sys.stdout.write(w+"\r["+g+"INFO"+w+"]"+g+" Verbindung wird zu"+w+" %s "%(HOST)+g+"über den Port "+w+"%s"%(PORT)+g+" hergestellt..")
 sys.stdout.flush()
 BUFSIZ = 1024
@@ -102,7 +117,8 @@ except:
     quit()
 print(w+"["+g+"INFO"+w+"]"+w+" Eigene"+g+" AES-Verschlüsselung wird benutzt")
 print(w+"["+g+"INFO"+w+"]"+g+" Sicherer Chatroom ist bereit")
+print(w+"["+g+"INFO"+w+"]"+g+" Dein Nickname: %s"%(nickname))
+sendnickname(nickname)
 receive_thread = Thread(target=receive)
 receive_thread.start()
 tkinter.mainloop()
-
