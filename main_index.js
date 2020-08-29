56b7763db5d6771ce5b5e4829c3c01360c271514
Pulsar <62395480+Woodnet@users.noreply.github.com>
2020-08-29 22:41:36 +0200

    Update main_index.js

--- main_index.js
+++ main_index.js
@@ -1,12 +1,19 @@
 var auto_refresh = setInterval(
         function ()
         {
-          $('#content1').load('chatindex.html');
+          $('#content1').load('changeindex.html');
         }, 1000);
 function CopyFunction() {
           var copyText = document.getElementById("encryptionkey");
           copyText.select();
           copyText.setSelectionRange(0, 99999); 
           document.execCommand("copy");
-          alert("Copied the Encryption Key!");
+          alert("Copied the Chatserver-Encryption Key!");
+} 
+function CopyFunction2() {
+  var copyText = document.getElementById("encryptionkey2");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); 
+  document.execCommand("copy");
+  alert("Copied the Sec-Network-Encryption Key!");
 } 
